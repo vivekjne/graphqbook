6c1c97fb4b5e11d37f3e65ee5d2f09e39fe5ad93
vivekjoy <vivek.joy@litmus7.com>
2020-08-15 01:45:57 +0530

    completed setting up basic resolvers

---
+++ src/server/services/graphql/schema.js
@@ -0,0 +1,41 @@
+const typeDefinitions = `
+
+    type User {
+        avatar:String
+        username:String
+    }
+
+    type Post {
+        id: Int
+        text: String
+        user:User
+    }
+
+    input PostInput {
+        text:String!
+    }
+
+    input UserInput {
+        username:String!
+        avatar: String!
+    }
+
+
+    type RootQuery {
+        posts: [Post]
+    }
+
+    type RootMutation {
+        addPost(
+            post:PostInput!
+            user:UserInput!
+            ):Post
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`;
+
+export default [typeDefinitions];
