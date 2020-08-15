549a4a31836682981bfb9a24711bcdd2c0a8bc6c
vivekjoy <vivek.joy@litmus7.com>
2020-08-15 11:51:54 +0530

    sequelize setup completed

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import cors from "cors";
 import compress from "compression";
 import services from "./services";
+import db from "./database";
 
 const root = path.join(__dirname, "../../");
 
